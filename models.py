87578541bb7aeda607b2c9260fc5e0b162e62179
MasazI <masaz.dream@gmail.com>
2017-02-07 02:18:45 +0900

    cnn for cifar10

--- models.py
+++ models.py
@@ -3,7 +3,8 @@ from __future__ import division
 from __future__ import print_function
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
 
 def mlp():
     model = Sequential()
@@ -15,4 +16,32 @@ def mlp():
     model.add(Dropout(0.2))
     model.add(Dense(10))
     model.add(Activation('softmax'))
+    return model
+
+
+def cnn(input_shape, nb_classes=10):
+    model = Sequential()
+
+    model.add(Convolution2D(32, 3, 3, border_mode='same',
+                            input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(64, 3, 3, border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+
     return model
