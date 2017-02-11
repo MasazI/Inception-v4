38ab7485991808d62fbdac6ba1c36cc1d2790467
MasazI <masaz.dream@gmail.com>
2017-02-11 11:57:45 +0900

    add realtime data augmentation

--- task.py
+++ task.py
@@ -3,11 +3,16 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import cv2
+import numpy as np
 import time
 import argparse
 import batches
 from keras.optimizers import Adam
+import keras.preprocessing.image as keras_image
 from inception_v4 import create_inception_v4
+
+DEBUG = False
 
 def eval():
     pass
@@ -52,11 +57,6 @@ def train(**kwargs):
 
         model.compile(optimizer=opt, loss='categorical_crossentropy')
 
-        # try:
-        #     model.load_weights(filepath=os.path.join('weights', 'inception-v4_weights_tf_dim_ordering_tf_kernels.h5'))
-        # except Exception as e:
-        #     print("[Error] %s" % e)
-
         model_dir = os.path.join("models", model_name)
         if not os.path.exists(model_dir):
             os.makedirs(model_dir)
@@ -67,18 +67,53 @@ def train(**kwargs):
             BatchGen = DataGen.gen_batch()
             for batch in BatchGen:
                 X_batch, Y_batch = batch
-                train_loss = model.train_on_batch(X_batch, Y_batch)
+                if DEBUG:
+                    print("X batch shape: %s" % (X_batch.shape,))
+
+                X_batch_list = []
+                for i, x in enumerate(X_batch):
+                    if DEBUG:
+                        print("batch shape: %s" % (x.shape,))
+                    # rotation
+                    x = keras_image.random_rotation(x, rg=40, row_axis=2, col_axis=1, channel_axis=2)
+                    # shift
+                    x = keras_image.random_shift(x, 0.1, 0.1, row_axis=2, col_axis=1, channel_axis=2)
+                    # share
+                    x = keras_image.random_shear(x, 0.2, row_axis=2, col_axis=1, channel_axis=2)
+                    # zoom
+                    x = keras_image.random_zoom(x, (0.7, 1.3), row_axis=2, col_axis=1, channel_axis=2)
+                    # flip horizontal
+                    if np.random.random() < 0.5:
+                        x = keras_image.flip_axis(x, axis=1)
+
+                    if DEBUG:
+                        x_disp = x[:,:,::-1]
+                        cv2.imshow("image", x_disp)
+                        cv2.waitKey(0)
+                        print("batch shape: %s" % (x.shape,))
+                    X_batch_list.append(x)
+                arr_img = np.array(X_batch_list)
+
+                if DEBUG:
+                    print("X batch aug shape: %s" % (arr_img.shape,))
+                    for sample in arr_img:
+                        sample_disp = sample[:,:,::-1]
+                        cv2.imshow("image", sample_disp)
+                        cv2.waitKey(0)
+
+                train_loss = model.train_on_batch(arr_img, Y_batch)
                 batch_counter += 1
                 if batch_counter >= n_batch_per_epoch:
                     print('Train loss: %f' % train_loss)
                     break
             print('Epoch %s/%s, Time: %s' % (epoch + 1, nb_epoch, time.time() - start))
 
-            if epoch % 5 == 0:
+            if epoch % 20 == 0:
                 weights_path = os.path.join('models/%s/%s_weights_epoch%s.h5' %
                                             (model_name, model_name, epoch))
                 model.save_weights(weights_path, overwrite=True)
-    except:
+    except Exception as e:
+        print(e)
         pass
 
 
@@ -97,7 +132,7 @@ if __name__ == '__main__':
     # parser.add_argument('--training_mode', default="in_memory", type=str,
     #                     help=('Training mode. Choose in_memory to load all the data in memory and train.'
     #                           'Choose on_demand to load batches from disk at each step'))
-    parser.add_argument('--batch_size', default=10, type=int, help='Batch size')
+    parser.add_argument('--batch_size', default=20, type=int, help='Batch size')
     parser.add_argument('--n_batch_per_epoch', default=100, type=int, help="Number of training epochs")
     parser.add_argument('--nb_epoch', default=400, type=int, help="Number of batches per epoch")
     parser.add_argument('--epoch', default=0, type=int, help="Epoch at which weights were saved for evaluation")
